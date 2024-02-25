d2c372e9d66cb6985faedc63a11bb6ef7411ad3a
Marc Jenald Galino <galinomarcjenald20@gmail.com>
2024-02-25 11:33:54 +0800

    Add scroll bars styling in Home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -18,7 +18,7 @@ defineProps({});
         >
             <div class="lg:grid lg:grid-cols-12 lg:gap-12">
                 <div
-                    class="lg:col-span-3 h-[calc(100vh-58px)] top-[58px] py-6 px-3 shadow sticky hidden lg:block overflow-auto"
+                    class="lg:col-span-3 h-[calc(100vh-58px)] top-[58px] px-3 sticky hidden lg:block overflow-auto"
                 >
                     <GroupList />
                 </div>
@@ -27,7 +27,7 @@ defineProps({});
                     <PostList />
                 </div>
                 <div
-                    class="lg:col-span-3 h-[calc(100vh-58px)] top-[58px] py-6 px-3 shadow sticky hidden lg:block overflow-auto"
+                    class="lg:col-span-3 h-[calc(100vh-58px)] top-[58px] px-3 sticky hidden lg:block overflow-auto"
                 >
                     <FollowingList />
                 </div>
@@ -36,4 +36,18 @@ defineProps({});
     </AuthenticatedLayout>
 </template>
 
-<style></style>
+<style>
+/* Webkit (Safari/Chrome) */
+::-webkit-scrollbar {
+    width: 8px;
+}
+::-webkit-scrollbar-track {
+    @apply bg-gray-100 dark:bg-gray-900;
+}
+::-webkit-scrollbar-thumb {
+    @apply bg-gray-300 dark:bg-gray-800 rounded-full;
+}
+::-webkit-scrollbar-thumb:hover {
+    @apply bg-gray-500 dark:bg-gray-700;
+}
+</style>
